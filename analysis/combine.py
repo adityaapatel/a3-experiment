8a48d00f6eb3e06d845fba332dadd03cfc61d188
Vic <vjradina@wpi.edu>
2026-02-19 16:02:58 -0500

    combine csv's and log base 2 formula

---
+++ analysis/combine.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import glob as glob
+import numpy as np
+
+
+files = glob.glob("data/a3*.csv")
+
+df = pd.concat([pd.read_csv(f) for f in files])
+
+df["Error"] = np.log2(np.abs(df["ReportedPercent"]/ df["TruePercent"]) + 0.125)
+df["Error"] = df["Error"].where(df["ReportedPercent"] != df["TruePercent"], 0)
+
+df.to_csv("data/master.csv", index=False)
+
+print(f"Combined {len(files)} files, {len(df)} total trials ")
