64abdd8ef379104ade655f6944cb5bcfb069c2e6
Karinne Aiello <164554084+karinneaiello@users.noreply.github.com>
2026-02-19 16:33:53 -0500

    Updated terms

--- analysis/combine.py
+++ analysis/combine.py
@@ -3,13 +3,13 @@ import glob as glob
 import numpy as np
 
 
-files = glob.glob("data/a3*.csv")
+files = glob.glob("trials/a3*.csv")
 
 df = pd.concat([pd.read_csv(f) for f in files])
 
 df["Error"] = np.log2(np.abs(df["ReportedPercent"]/ df["TruePercent"]) + 0.125)
 df["Error"] = df["Error"].where(df["ReportedPercent"] != df["TruePercent"], 0)
 
-df.to_csv("data/master.csv", index=False)
+df.to_csv("trials/master.csv", index=False)
 
 print(f"Combined {len(files)} files, {len(df)} total trials ")
