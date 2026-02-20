ebf394e0662b31c9b25a3b04a480ee64fd5e884a
Karinne Aiello <164554084+karinneaiello@users.noreply.github.com>
2026-02-19 21:59:29 -0500

    Fixed equation

--- analysis/combine.py
+++ analysis/combine.py
@@ -7,7 +7,7 @@ files = glob.glob("trials/a3*.csv")
 
 df = pd.concat([pd.read_csv(f) for f in files])
 
-df["Error"] = np.log2(np.abs(df["ReportedPercent"]/ df["TruePercent"]) + 0.125)
+df["Error"] = np.log2(np.abs(df["ReportedPercent"] - df["TruePercent"]) + 0.125)
 df["Error"] = df["Error"].where(df["ReportedPercent"] != df["TruePercent"], 0)
 
 df.to_csv("trials/master.csv", index=False)
