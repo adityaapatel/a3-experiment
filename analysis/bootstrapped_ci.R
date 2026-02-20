602ac39a30edc5c3a86bb623f1b9c9a3e06641ed
Karinne Aiello <164554084+karinneaiello@users.noreply.github.com>
2026-02-19 19:20:47 -0500

    Make 95% bootstrapped CIs

---
+++ analysis/bootstrapped_ci.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+
+# load data
+df <- read.csv("trials/master.csv")
+
+# do absolute value of errors
+df$Error <- abs(df$Error)
+
+# filter by vis type
+df$type <- factor(
+  sub("_[0-9]+$", "", df$Vis),
+  levels = c("bar", "stacked", "donut")
+)
+
+# plot with mean and 95% bootstrapped CI
+r_plot <- ggplot(df, aes(x = Error, y = type)) +
+  
+  # mean point
+  stat_summary(
+    fun = mean,
+    geom = "point",
+    size = 3,
+    color = "black"
+  ) +
+  
+  # bootstrapped 95% CI
+  stat_summary(
+    fun.data = mean_cl_boot,
+    geom = "errorbarh",
+    height = 0.2,
+    color = "black"
+  ) +
+  
+  labs(
+    x = "Log Error",
+    y = "Vis Type",
+    title = "Mean Absolute Error by Vis Type with 95% Bootstrapped CI"
+  ) +
+  
+  theme_minimal(base_size = 12)
+
+r_plot
